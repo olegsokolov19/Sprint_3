69c41858a563e4aa8ac68d86626ae390b272af58
Oleg Sokolov <olegsokolov190@44-4.ru>
2022-05-29 16:49:18 +0600

    Add Parameterized class

--- src/test/java/CreateOrderTest.java
+++ src/test/java/CreateOrderTest.java
@@ -4,12 +4,29 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import ru.yandex.practicum.scooter.api.model.OrderModel;
 
 import static org.apache.http.HttpStatus.SC_CREATED;
-import static org.hamcrest.CoreMatchers.notNullValue;
 
+@RunWith(Parameterized.class)
 public class CreateOrderTest {
+    private final String[] color;
+
+    public CreateOrderTest(String[] color) {
+        this.color = color;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getColor() {
+        return new Object[][]{
+                {new String[]{"BLACK"}},
+                {new String[]{"GREY"}},
+                {new String[]{"BLACK", "GREY"}},
+                {null}
+        };
+    }
 
     Order order = CreateRandomOrder.getRandomOrder();
     OrderModel orderModel;
@@ -19,48 +36,19 @@ public class CreateOrderTest {
     @Before
     public void setUp() {
         RestAssured.requestSpecification = requestSpec;
+
         orderModel = new OrderModel(order.getFirstName(), order.getLastName(), order.getAddress(), order.getMetroStation(),
                 order.getPhone(), order.getRentTime(), order.getDeliveryDate(), order.getComment());
     }
 
     @Test
-    @DisplayName("Можно создать заказ на самокат чёрного цвета")
-    @Description("Проверяем, что при создании заказа можно заказать самокат чёрного цвета")
+    @DisplayName("Можно создать заказ на самокат с выбранным цветом или без выбора цвета")
+    @Description("Проверяем, что при создании заказа можно заказать чёрный или серый самокат, чёрный и серый сразу или вообще не указывать цвет")
     public void createOrderWithBlackColor() {
-        orderModel.setColor(new String[]{"BLACK"});
+        orderModel.setColor(color);
 
         order.createOrder(orderModel)
                 .then().statusCode(SC_CREATED);
-    }
-
-    @Test
-    @DisplayName("Можно создать заказ на самокат серого цвета")
-    @Description("Проверяем, что при создании заказа можно заказать самокат серого цвета")
-    public void createOrderWithGreyColor() {
-        orderModel.setColor(new String[]{"GREY"});
-
-        order.createOrder(orderModel)
-                .then().statusCode(SC_CREATED);
-    }
-
-    @Test
-    @DisplayName("Можно создать заказ на самокаты чёрного серого цветов")
-    @Description("Проверяем, что при создании заказа можно заказать самокаты чёрного и серого цветов")
-    public void createOrderWithBlackAndGreyColor() {
-        orderModel.setColor(new String[]{"BLACK", "GREY"});
-
-        order.createOrder(orderModel)
-                .then().statusCode(SC_CREATED);
-    }
-
-    @Test
-    @DisplayName("Тело ответа содержит \"track\"")
-    @Description("Проверяем, что при создании заказа в теле ответа приходит поле \"track\"")
-    public void responseBodyHasTrackField() {
-        orderModel.setColor(new String[]{"BLACK", "GREY"});
-
-        order.createOrder(orderModel)
-                .then().body("track", notNullValue());
     }
 
 }
